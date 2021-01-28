6d60f4021cd3b2fd446e9cfc5f61131e28b29873
JunjieHU <jeremy.junjie.hu@gmail.com>
2021-01-28 12:41:04 +0100

    more epochs, add comments. Current tensorflow reduces the training time to around 2 minutes; change path of files to save

--- DEDA_MachineLearning/ConvolutionalNeuralNetwork_mnist.py
+++ DEDA_MachineLearning/ConvolutionalNeuralNetwork_mnist.py
@@ -17,14 +17,14 @@ from tensorflow.keras.models import load_model
 
 
 def trainning_process(model_history):
-    fig = plt.figure(figsize=(15, 5))
+    fig = plt.figure(figsize=(16, 5))
     # Accuracy Increasing
     plt.subplot(1, 2, 1)
     plt.plot(range(1, epochs + 1), model_history.history['accuracy'], 'blue')
     plt.plot(range(1, epochs + 1), model_history.history['val_accuracy'], 'red')
     plt.title('Model Accuracy')
     plt.ylabel('Accuracy', fontsize=15)
-    plt.yticks(fontsize=18)
+    plt.yticks(fontsize=15)
     plt.xlabel('Epoch', fontsize=15)
     plt.xticks(np.arange(1, epochs + 1), fontsize=15)
     # Loss Decreasing
@@ -32,9 +32,11 @@ def trainning_process(model_history):
     plt.plot(range(1, epochs + 1), model_history.history['loss'], 'blue')
     plt.plot(range(1, epochs + 1), model_history.history['val_loss'], 'red')
     plt.title('Model Loss')
-    plt.ylabel('Loss', fontsize=15)
+    plt.ylabel('Categorical Cross-Entropy', fontsize=15)
+    plt.yticks(fontsize=15)
     plt.xlabel('Epoch', fontsize=15)
     plt.xticks(np.arange(1, epochs + 1), fontsize=15)
+    plt.tight_layout()
     plt.show()
     return fig
 
@@ -57,8 +59,8 @@ pool_size = (2, 2)
 kernel_size = (3, 3)
 input_shape = (row_size, col_size, 1)
 num_classes = 10
-batch_size = 128
-epochs = 12
+batch_size = 128  # Number of sample used to update gradient
+epochs = 24  # Number of iterations
 
 # data pre-processing
 X_train = X_train.reshape(train_sample_size, *input_shape).astype('float32')
@@ -98,17 +100,21 @@ model.compile(optimizer='adam',
 
 # training
 start_time = time.time()
-model_result = model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1,
+model_result = model.fit(X_train, y_train,
+                         batch_size=batch_size,
+                         epochs=epochs,
+                         verbose=1,
                          validation_data=(X_test, y_test))
 end_time = time.time()
+print(f'Training takes {round((end_time - start_time) / 60, 1)} minutes to complete')
 process_plot = trainning_process(model_history=model_result)
-process_plot.savefig('trainning_process_plot.png', dpi=300)
-print(f'Training takes {round((end_time - start_time)/60, 1)} minutes to complete')  # Takes about half an hour to finish
+process_plot.savefig('DEDA_MachineLearning/trainning_process_plot.png', dpi=300)
 validation_acc = model_result.history['val_accuracy'][-1]
-print(f'The final accuracy is {validation_acc*100}%')  # The final cross validation accuracy is 99.22%
-model.save('cnn_model.h5')
+print(f'The final accuracy is {validation_acc * 100}%')
+# The final cross validation accuracy is 94.24%
+model.save('DEDA_MachineLearning/cnn_model.h5')
 
 # ============Load Trained Model=============
-model_loaded = load_model('cnn_model.h5')
+model_loaded = load_model('DEDA_MachineLearning/cnn_model.h5')
 test_accu = model_loaded.evaluate(X_test, y_test)
 print(f'Test Accuracy is: {test_accu[1]}')
