0e09044cdca56707f15409b8add1e39cc6b62f74
JunjieHU <jeremy.junjie.hu@gmail.com>
2021-09-19 18:06:18 +0200

    renew main file and delete readme for auto updating

--- DEDA_WebScrapingIntro/DEDA_WebScrapingIntro.py
+++ DEDA_WebScrapingIntro/DEDA_WebScrapingIntro.py
@@ -46,48 +46,95 @@ from bs4 import BeautifulSoup as soup
 import datetime
 import pandas as pd
 import os
+import pickle
 
 # Using the requests module to get source code from the url
-nasdaq_url = 'http://www.nasdaq.com/news/market-headlines.aspx'
-url_request = requests.get(nasdaq_url)
+nasdaq_url = 'https://www.nasdaq.com/news-and-insights/topic/markets/stocks/page/1'
+
+direct = os.getcwd() + '/DEDA_WebScrapingIntro/'
+# Or:
+# direct = os.getcwd()
+
+refresh = True
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36',
+    "Upgrade-Insecure-Requests": "1", "DNT": "1",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": "gzip, deflate"}
+# Header is used to deceive the webpage host server that we are visiting the webpage via a browser
+
+if (not os.path.exists(direct + 'temp/' + '0.pkl')) or (refresh is True):
+    # connect to the website if the webpage source code file is not exist or we need to refresh it
+    url_request = requests.get(nasdaq_url, headers=headers)
+    status_code = url_request.status_code
+
+    if status_code != 200:
+        raise ConnectionRefusedError("Unable to connect to the website")
+    # save the request object after scraping
+    with open(direct + 'temp/' + '0.pkl', 'wb') as url_file:
+        pickle.dump(url_request, url_file)
+else:
+    # else, we open the source code file from local disk to save time
+    with open(direct + 'temp/' + '0.pkl', 'rb') as url_file:
+        url_request = pickle.load(url_file)
+
 url_content = url_request.content
 # Using BeautifulSoup to parse webpage source code
-parsed_content = soup(url_content)
-# Finding all the <p> tag content
-containers = parsed_content.find_all('p')
+parsed_content = soup(url_content, "html.parser")
 
-# initial empty list to load the data
-link_list = []
-title_list = []
-time_list = []
-tag_list = []
+# Using .find_all() method to search tag name and attribute
+containers = parsed_content.find_all('div', {'data-type': 'article'})
+page_info = list()  # initial empty list to load the data
 
 for container in containers:
-    # Using .find_all() method to search tag name and attribute
-    container_a = container.find_all('a', {"id": "two_column_main_content_la1_rptArticles_hlArticleLink_0"})
-    for item in container_a:
-        # Loop in the <a> tag
-        news_link = item.get('href').strip()  # strip() method can remove specific chars at the head and tail
-        news_title = item.text.strip()
-        link_list.append(news_link)
-        title_list.append(news_title)
-    container_span = container.find_all('span', {'class': 'small'})
-    for item in container_span:
-        item_split = item.text.split(' - ')
-        print(item_split)
-        time = datetime.datetime.strptime(item_split[0], '%m/%d/%Y %I:%M:%S %p')
-        # Formatting date see further more: https://docs.python.org/3/library/datetime.html#strftime-and-strptime-behavior
-        time_list.append(time)
+    """
+    Scrape 1 item each time, each item contains title, time, author_name, tag, author_url 
+    """
+    unit_info = dict()
+
+    # Find title item
+    container_title = container.find_all('a', {"class": "content-feed__card-title-link"})
+
+    if container_title == []:
+        continue
+
+    news_link = container_title[0].get('href').strip()
+    news_link = f"https://www.nasdaq.com{news_link}"
+    news_title = container_title[0].text.strip()  # strip() method can remove specific chars at the head and tail
+    unit_info['news_title'] = news_title
+    unit_info['news_link'] = news_link
+
+    try:
         # Using try...except to catch unexpected condition so that program can keep running
+        # Formatting date see further more: https://docs.python.org/3/library/datetime.html#strftime-and-strptime-behavior
+
+        news_date = datetime.datetime.strptime(news_link[-10:], '%Y-%m-%d')
+        news_date = news_date.date()
+
+    except:
+        container_time = container.find_all('div', {'class': 'content-feed__card-timestamp'})
         try:
-            tag = item_split[1].split('  in ')[1].strip()
-        except Exception:
-            # If the tag doesn't exist, mark it as an empty list
-            tag = []
-        tag_list.append(tag)
-nasdaq_info = zip(title_list, time_list, link_list, tag_list)
-nasdaq_info_df = pd.DataFrame(list(nasdaq_info), columns=['title', 'time', 'link', 'tag'])
-nasdaq_info_df.to_csv(os.getcwd() + '/DEDA_Class_2017_WebScrapingIntro/Nasdaq_News.csv')
+            news_date = datetime.datetime.strptime(container_time[0].text.strip(), '%b %d,%Y')
+            news_date = news_date.date()
+
+        except:
+            t_day = datetime.date.today()
+            date_series = container_time[0].text.strip().split(' ')
+            if (date_series[1].startswith('hour')) or (date_series[1].startswith('minute')):
+                news_date = t_day
+
+            elif date_series[1].startswith('day'):
+                news_date = t_day - datetime.timedelta(days=int(date_series[0]))
+            else:
+                news_date = t_day
+
+    news_date = datetime.datetime(news_date.year, news_date.month, news_date.day)
+    unit_info['news_time'] = news_date
+    page_info.append(unit_info)
+
+nasdaq_info_df = pd.DataFrame(page_info, columns=['news_title', 'news_link', 'news_time'])
+nasdaq_info_df.to_csv(direct + '/Nasdaq_headline_onepage.csv')
 
 """
 NasdaqNewsScraping_Function.py
@@ -100,19 +147,20 @@ import os
 import pandas as pd
 import pickle
 
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36',
+    "Upgrade-Insecure-Requests": "1", "DNT": "1",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": "gzip, deflate"}
+# Header is used to deceive the webpage host server that we are visiting the webpage via a browser
 
 def nasdaq_news_scraping(page=1, refresh=False):
     # Argument page equals 1 by default
-    if page == 1:
-        # Visit the home page if page equals 1
-        nasdaq_url = 'http://www.nasdaq.com/news/market-headlines.aspx'
-    else:
-        # Change url by different argument
-        nasdaq_url = 'http://www.nasdaq.com/news/market-headlines.aspx?page=' + str(page)
+    nasdaq_url = f'https://www.nasdaq.com/news-and-insights/topic/markets/stocks/page/{page}'
 
     if (not os.path.exists(direct + '/temp/' + str(page) + '.pkl')) or (refresh is True):
         # connect to the website if the webpage source code file is not exist of we need to refresh it
-        url_request = requests.get(nasdaq_url)
+        url_request = requests.get(nasdaq_url, headers=headers)
         # save the request object after scraping
         with open(direct + '/temp/' + str(page) + '.pkl', 'wb') as url_file:
             pickle.dump(url_request, url_file)
@@ -121,57 +169,68 @@ def nasdaq_news_scraping(page=1, refresh=False):
         with open(direct + '/temp/' + str(page) + '.pkl', 'rb') as url_file:
             url_request = pickle.load(url_file)
     url_content = url_request.content
-    parsed_content = soup(url_content)
-    containers = parsed_content.find_all('p')
-
-    title_list_page = []
-    time_list_page = []
-    link_list_page = []
-    tag_list_page = []
+    parsed_content = soup(url_content, "html.parser")
+    containers = parsed_content.find_all('div', {'data-type': 'article'})
+    page_info = list()
 
     for container in containers:
-        container_a = container.find_all('a', {"id": "two_column_main_content_la1_rptArticles_hlArticleLink_0"})
-        for item in container_a:
-            news_link = item.get('href').strip()
-            news_title = item.text.strip()
-            title_list_page.append(news_title)
-            print(news_title)
-            link_list_page.append(news_link)
-            print(news_link)
-        container_span = container.find_all('span', {'class': 'small'})
-        for item in container_span:
-            item_split = item.text.split(' - ', 1)
-            time = datetime.datetime.strptime(item_split[0], '%m/%d/%Y %I:%M:%S %p')
-            time_list_page.append(time)
-            print(time)
+        """
+        Scrape 1 item each time, each item contains title, time, author_name, tag, author_url 
+        """
+        unit_info = dict()
+
+        container_title = container.find_all('a', {"class": "content-feed__card-title-link"})
+        if container_title == []:
+            continue
+
+        news_link = container_title[0].get('href').strip()
+        news_link = f"https://www.nasdaq.com{news_link}"
+        news_title = container_title[0].text.strip()
+        unit_info['news_title'] = news_title
+        unit_info['news_link'] = news_link
+
+        try:
+            news_date = datetime.datetime.strptime(news_link[-10:], '%Y-%m-%d')
+            news_date = news_date.date()
+
+        except:
+            container_time = container.find_all('div', {'class': 'content-feed__card-timestamp'})
             try:
-                tag = item_split[1].split('  in ')[1].strip()
-            except Exception:
-                tag = []
-            tag_list_page.append(tag)
-            print(tag)
-    return title_list_page, time_list_page, link_list_page, tag_list_page
+                news_date = datetime.datetime.strptime(container_time[0].text.strip(), '%b %d,%Y')
+                news_date = news_date.date()
+
+            except:
+                t_day = datetime.date.today()
+                date_series = container_time[0].text.strip().split(' ')
+                if (date_series[1].startswith('hour')) or (date_series[1].startswith('minute')):
+                    news_date = t_day
+
+                elif date_series[1].startswith('day'):
+                    news_date = t_day - datetime.timedelta(days=int(date_series[0]))
+                else:
+                    news_date = t_day
+
+        news_date = datetime.datetime(news_date.year, news_date.month, news_date.day)
+        unit_info['news_time'] = news_date
+        page_info.append(unit_info)
+
+    return page_info
 
 
-direct = os.getcwd() + '/DEDA_Class_2017_WebScrapingIntro'
+direct = os.getcwd() + '/DEDA_WebScrapingIntro/'
+# Or
+# direct = os.getcwd()
 
-title_list = []
-time_list = []
-link_list = []
-tag_list = []
+head_lines_info = list()
 
-for page_num in range(1, 10):
-    print("\nThis is Page: ", page_num)
+for page_num in range(1, 30):
+    print("This is Page: ", page_num)
     # Using the function defined previously with certain arguments as input
-    nasdaq_news_page = nasdaq_news_scraping(page=page_num, refresh=False)
-    title_list.extend(nasdaq_news_page[0])
-    time_list.extend(nasdaq_news_page[1])
-    link_list.extend(nasdaq_news_page[2])
-    tag_list.extend(nasdaq_news_page[3])
+    page_info = nasdaq_news_scraping(page=page_num, refresh=True)
+    head_lines_info.extend(page_info)
 
-nasdaq_info = zip(title_list, time_list, link_list, tag_list)
-nasdaq_info_df = pd.DataFrame(list(nasdaq_info), columns=['title', 'time', 'link', 'tag'])
-nasdaq_info_df.to_csv(direct + '/Nasdaq_News_MultiPages.csv')
+headlines_info_df = pd.DataFrame(head_lines_info, columns=['news_title', 'news_link', 'news_time'])
+headlines_info_df.to_csv(direct + '/Nasdaq_News_MultiPages.csv')
 
 """
 ChinaCities_WeatherPageScraping.py
