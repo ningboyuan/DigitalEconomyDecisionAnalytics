3438bededd381c08c4dc1b8d7cbe5f8f3454e1c9
Ramona <59314862+s6rrmerk@users.noreply.github.com>
2021-10-16 14:17:05 +0200

    Update ReadXML.py

--- DEDA_WebScrapingIntro/ReadXML.py
+++ DEDA_WebScrapingIntro/ReadXML.py
@@ -10,5 +10,3 @@ response = requests.get(
     "https://www.treasury.gov/resource-center/data-chart-center/interest-rates/Datasets/daily_treas_bill_rates.xml")
 content = response.content
 dataDOM = xml.dom.minidom.parseString(content)
-response = requests.get(
-    "https://news.google.com/news/rss/headlines/section/q/finance%20news/finance%20news?ned=us&hl=en")
